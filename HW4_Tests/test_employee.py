ee689aa62ba473711c39ee86f4432f4c34e88524
mariana <mariasyad@gmail.com>
2021-05-13 18:44:22 +0300

    finished test_employee.py

--- HW4_Tests/test_employee.py
+++ HW4_Tests/test_employee.py
@@ -1,15 +1,14 @@
 import unittest
 from unittest.mock import patch
 from employee import Employee
-import requests
 
 
 class TestEmployee(unittest.TestCase):
     def setUp(self) -> None:
         self.employee1 = Employee("John", "Lennon", 1000)
-        self.employee2 = Employee("Paul", "McCartney", 1500)
-        self.employee3 = Employee("Ringo", "Starr", 1350)
-        self.employee4 = Employee("George", "Harrison", 1200)
+        self.employee2 = Employee("Paul", "McCartney", -1500)
+        self.employee3 = Employee("Ringo", "Starr", 0)
+        self.employee4 = Employee("George", "Harrison", None)
 
     def test_email(self):
         self.assertEqual(self.employee1.email, "John.Lennon@email.com")
@@ -27,9 +26,22 @@ class TestEmployee(unittest.TestCase):
         self.employee1.apply_raise()
         self.assertEqual(self.employee1.pay, 1050)
         self.employee2.apply_raise()
-        self.assertEqual(self.employee2.pay, 1575)
+        self.assertEqual(self.employee2.pay, -1575)
         self.employee3.apply_raise()
-        self.assertEqual(self.employee3.pay, 1417)
-        self.employee4.apply_raise()
-        self.assertEqual(self.employee4.pay, 1260)
+        self.assertEqual(self.employee3.pay, 0)
+        with self.assertRaises(TypeError):
+            self.employee4.apply_raise()
+        self.assertEqual(self.employee4.pay, None)
 
+    @patch('employee.requests.get')
+    def test_monthly_schedule(self, mock_get_response):
+        mock_get_response.return_value.ok = True
+        self.assertEqual(self.employee1.monthly_schedule("March"), mock_get_response().text)
+        self.assertEqual(self.employee2.monthly_schedule("June"), mock_get_response().text)
+        mock_get_response.return_value.ok = False
+        self.assertEqual(self.employee3.monthly_schedule("July"), "Bad Response!")
+        self.assertEqual(self.employee4.monthly_schedule("January"), "Bad Response!")
+
+
+if __name__ == "__main__":
+    unittest.main()
